0a6539c768dd245c36056b8e6a3b1f902a3e571b
farrasdoko <farras.abiyyu.doko@gmail.com>
2020-08-05 06:27:35 +0700

    Create new project...

---
+++ WufWuf/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WufWuf
+//
+//  Created by Farras Doko on 05/08/20.
+//  Copyright © 2020 wufwuf. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
