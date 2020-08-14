902b51b9a2731b5e07eda9162790ca651cc8db93
farrasdoko <farras.abiyyu.doko@gmail.com>
2020-08-14 21:17:20 +0700

    - tab bar deleted - image to right-top corner - add diagnosa segment - add read more

--- WufWuf/Controller/Article/ArticleController.swift
+++ WufWuf/Controller/Article/ArticleController.swift
@@ -14,6 +14,7 @@ class ArticleController: UIViewController {
     @IBOutlet weak var bannerImg: UIImageView!
     @IBOutlet weak var segmented: UISegmentedControl!
     @IBOutlet weak var detailLabel: UITextView!
+    @IBOutlet weak var readMore: UIButton!
     
     var navTitle: String?
     var detail = NSMutableAttributedString()
@@ -23,8 +24,33 @@ class ArticleController: UIViewController {
         
         // Do any additional setup after loading the view.
         navigationItem.title = navTitle
+        
+        switch navTitle {
+        case "Katarak":
+            let cameraBarItem = UIBarButtonItem(barButtonSystemItem: .camera, target: self, action: #selector(goToKatarakScr))
+            navigationItem.setRightBarButton(cameraBarItem, animated: false)
+            break
+        case "Demodex":
+            let cameraBarItem = UIBarButtonItem(barButtonSystemItem: .camera, target: self, action: #selector(goToDemodexScr))
+            navigationItem.setRightBarButton(cameraBarItem, animated: false)
+            break
+        default:
+            break
+        }
+        
         changeDetail()
     }
+    
+    @objc func goToDemodexScr() {
+        let scanDemodexController:UIViewController = UIStoryboard(name: "DemoDexScannerScreen", bundle: nil).instantiateViewController(withIdentifier: "main")
+        present(scanDemodexController, animated: true, completion: nil)
+    }
+    
+    @objc func goToKatarakScr() {
+        let scanKatarakController:UIViewController = UIStoryboard(name: "ScannerScreen", bundle: nil).instantiateViewController(withIdentifier: "ScannerScreen1") as UIViewController
+        present(scanKatarakController, animated: true, completion: nil)
+    }
+    
     
     func getArticle(_ detail: [Any]) -> NSMutableAttributedString {
         let string = NSMutableAttributedString()
@@ -59,15 +85,24 @@ class ArticleController: UIViewController {
             switch segmented.titleForSegment(at: segmented.selectedSegmentIndex) {
             case "Pengertian":
                 detailLabel.attributedText = getArticle(katarakPengertian)
+                detail = getArticle(katarakPengertian)
                 bannerImg.image = UIImage(named: "pengertian-katarak")
+                readMore.isHidden = false
                 break
             case "Penyebab":
                 detailLabel.attributedText = getArticle(katarakPenyebab)
                 bannerImg.image = UIImage(named: "penyebab-katarak")
+                readMore.isHidden = true
                 break
             case "Gejala":
                 detailLabel.attributedText = getArticle(katarakGejala)
                 bannerImg.image = UIImage(named: "gejala-katarak")
+                readMore.isHidden = true
+                break
+            case "Diagnosa":
+                detailLabel.attributedText = getArticle(katarakDiagnosa)
+                bannerImg.image = UIImage(named: "penyebab-katarak")
+                readMore.isHidden = true
                 break
             default:
                 break
@@ -78,14 +113,58 @@ class ArticleController: UIViewController {
             case "Pengertian":
                 detailLabel.attributedText = getArticle(demodexPengertian)
                 bannerImg.image = UIImage(named: "demodex-pengertian")
+                detail = getArticle(demodexPengertian)
+                readMore.isHidden = false
                 break
             case "Penyebab":
                 detailLabel.attributedText = getArticle(demodexPenyebab)
                 bannerImg.image = UIImage(named: "demodex-penyebab")
+                readMore.isHidden = true
                 break
             case "Gejala":
                 detailLabel.attributedText = getArticle(demodexGejala)
                 bannerImg.image = UIImage(named: "demodex-gejala")
+                detail = getArticle(demodexGejala)
+                readMore.isHidden = false
+                break
+            case "Diagnosa":
+                detailLabel.attributedText = getArticle(demodexDiagnosa)
+                bannerImg.image = UIImage(named: "demodex-penyebab")
+                readMore.isHidden = true
+                break
+            default:
+                break
+            }
+        default:
+            break
+        }
+    }
+    @IBAction func readMoreTapped(_ sender: UIButton) {
+        switch navTitle {
+        case "Katarak":
+            switch segmented.titleForSegment(at: segmented.selectedSegmentIndex) {
+            case "Pengertian":
+                let attr = NSAttributedString(string: kPengertianMore, attributes: [.font:UIFont.systemFont(ofSize: 17)])
+                detail.append(attr)
+                detailLabel.attributedText = detail
+                readMore.isHidden = true
+                break
+            default:
+                break
+            }
+        case "Demodex":
+            switch segmented.titleForSegment(at: segmented.selectedSegmentIndex) {
+            case "Pengertian":
+                let attr = NSAttributedString(string: dPengertianMore, attributes: [.font:UIFont.systemFont(ofSize: 17)])
+                detail.append(attr)
+                detailLabel.attributedText = detail
+                readMore.isHidden = true
+                break
+            case "Gejala":
+                let attr = NSAttributedString(string: dGejalaMore, attributes: [.font:UIFont.systemFont(ofSize: 17)])
+                detail.append(attr)
+                detailLabel.attributedText = detail
+                readMore.isHidden = true
                 break
             default:
                 break
