49421133f71fe9f3a957223b9cf52a615c00ecf4
Michael Tamsil <michaeltamsil@gmail.com>
2020-09-11 18:38:28 +0700

    add functions to use photo libraries and can crop

--- WufWuf/Controller/Cataract/Scanner/ScannerScreenViewController.swift
+++ WufWuf/Controller/Cataract/Scanner/ScannerScreenViewController.swift
@@ -9,15 +9,14 @@
 import UIKit
 import CoreML
 import Vision
+import AVFoundation
 
 class ScannerScreenViewController : UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     let imagePicker = UIImagePickerController()
     
     @IBOutlet weak var imageView: UIImageView!
-    
     @IBOutlet weak var judul: UILabel!
-    @IBOutlet weak var tombolPeta: UIButton!
     @IBOutlet weak var viewSaran: UIView!
     @IBOutlet weak var viewPenanganan: UIView!
     
@@ -25,32 +24,35 @@ class ScannerScreenViewController : UIViewController, UIImagePickerControllerDel
         super.viewDidLoad()
         
         imagePicker.delegate = self
-        imagePicker.allowsEditing = false
-        imagePicker.sourceType = .camera
+        imagePicker.allowsEditing = true
+        imagePicker.sourceType = .photoLibrary
         
         DispatchQueue.main.async {
-            self.tombolPeta.isHidden = true
             self.viewSaran.isHidden = true
             self.viewPenanganan.isHidden = true
         }
-        
     }
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-        
-        if let userPickedImage = info[.originalImage] as? UIImage {
+        if let userPickedImage = info[.editedImage] as? UIImage {
             imageView.image = userPickedImage
             
             guard let ciimage = CIImage(image: userPickedImage) else {
                 fatalError("could not convert UIImage into CIImage")
             }
             detect(ciimage)
+        } else {
+            print("can't take userPickedImage")
         }
         imagePicker.dismiss(animated: true, completion: nil)
-        
     }
     
     func detect(_ image: CIImage) {
+        DispatchQueue.main.async {
+            self.viewSaran.isHidden = true
+            self.viewPenanganan.isHidden = true
+        }
+        
         guard let model = try? VNCoreMLModel(for: CataractDogImageClassifier().model) else {
             fatalError("Loading CoreML Model Failed.")
         }
@@ -65,23 +67,23 @@ class ScannerScreenViewController : UIViewController, UIImagePickerControllerDel
             if let firstResult = results.first {
                 //MARK: PUT INFO HERE and determine what info will be
                 //self.contentLabel.text = firstResult.identifier
-                print(firstResult)
-                let isHealthy = firstResult.identifier == "Healthy"
+//                print(firstResult)
 
-                if (isHealthy){
-                    self.judul.text = "Wow, mata anjing anda sehat"
-                    DispatchQueue.main.async {
-                        self.tombolPeta.isHidden = true
-                        self.viewSaran.isHidden = true
-                        self.viewPenanganan.isHidden = true
-                    }
-                }else{
-                    self.judul.text = "Mata anjing anda memiliki penyakit katarak"
-                    DispatchQueue.main.async {
-                        self.tombolPeta.isHidden = false
-                        self.viewSaran.isHidden = false
-                        self.viewPenanganan.isHidden = false
-                    }
+                let category = firstResult.identifier
+                                
+                switch category {
+                    case "Cataract":
+                        self.judul.text = "Mata anjing anda memiliki penyakit katarak"
+                        DispatchQueue.main.async {
+                            self.viewSaran.isHidden = false
+                            self.viewPenanganan.isHidden = false
+                        }
+                        break
+                    case "Healthy":
+                        self.judul.text = "Wow, mata anjing anda sehat"
+                        break
+                    default:
+                        self.judul.text = "Maaf, gambar tidak dapat mendeteksi mata anjing. Coba ambil foto mata anjing sekali lagi"
                 }
                 
                 DispatchQueue.main.async {
@@ -100,6 +102,34 @@ class ScannerScreenViewController : UIViewController, UIImagePickerControllerDel
     }
     
     @IBAction func cameraTapped2(_ sender: Any) {
+        
+        func openCamera(){
+            imagePicker.sourceType = .camera
+            present(imagePicker, animated: true, completion: nil)
+        }
+        
+        switch AVCaptureDevice.authorizationStatus(for: .video) {
+            case .authorized:
+                openCamera()
+                break
+            default:
+                AVCaptureDevice.requestAccess(for: .video) { granted in
+                    if granted {
+                        openCamera()
+                    } else {
+                        DispatchQueue.main.async {
+                            let alert = UIAlertController(title: "permohonan penggunaan kamera", message: "mohon mengizinkan penggunaan kamera di menu setting, Halo Dog & hidupkan mode kamera", preferredStyle: .alert)
+                            alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Default action"), style: .default, handler: nil))
+                            self.present(alert, animated: true, completion: nil)
+                        }
+                    }
+                }
+                break
+        }
+    }
+    
+    @IBAction func photoTapped(_ sender: UIBarButtonItem) {
+        imagePicker.sourceType = .photoLibrary
         present(imagePicker, animated: true, completion: nil)
     }
 }
